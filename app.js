2ce856974b6513902ba1b385c71eda00e5c36389
Tatireddy Harshavardhan Reddy <48549064+tharshavardhanreddy@users.noreply.github.com>
2019-11-28 16:59:40 +0530

    fs.synand asyn

--- app.js
+++ app.js
@@ -7,9 +7,9 @@ console.log((count.add(5,6)));
 console.log(count.add(count.pi,5));
 
 var fs = require('fs');
-// var read = fs.readFileSync('readme.txt', 'utf8');
-// console.log(read);
-// fs.writeFileSync('writeme.txt',read);
+var read = fs.readFileSync('readme.txt', 'utf8');
+console.log(read);
+fs.writeFileSync('writeme.txt',read);
 
 fs.readFile('readme.txt','utf8',function(err,data){
     console.log(data);
