bbdcb776afb781c7eb5b2074f78b9c5cb4f0ed68
Tatireddy Harshavardhan Reddy <48549064+tharshavardhanreddy@users.noreply.github.com>
2019-11-29 12:21:03 +0530

    utf8

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var http = require('http');
 var fs = require('fs');
 
-var stream = fs.createReadStream(__dirname + '/readme.txt');
+var stream = fs.createReadStream(__dirname + '/readme.txt','utf8');
 stream.on('data',function(chunk){
     console.log('chunk arrived -------------------------------------------------------------------------------');
     console.log(chunk);
