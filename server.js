a93fd0a2ec09ced53ca72b49cd450e03a01a8133
Tatireddy Harshavardhan Reddy <48549064+tharshavardhanreddy@users.noreply.github.com>
2019-11-29 12:44:08 +0530

    write stream

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 var http = require('http');
 var fs = require('fs');
 
+// var stream = fs.createReadStream(__dirname + '/readme.txt','utf8');
+// stream.on('data',function(chunk){
+//     console.log('chunk arrived -------------------------------------------------------------------------------');
+//     console.log(chunk);
+// });
+
 var stream = fs.createReadStream(__dirname + '/readme.txt','utf8');
+var wstream = fs.createWriteStream(__dirname+'/writemeee.txt');
 stream.on('data',function(chunk){
     console.log('chunk arrived -------------------------------------------------------------------------------');
-    console.log(chunk);
+    wstream.write(chunk);
 });
 
 // var server = http.createServer(function(req,res){
